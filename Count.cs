361ca74180b17406b77bd4180a3af49929e52768
Tec <Tec@SKAB1-PC3>
2024-11-04 11:48:54 +0100

    Add project files.

---
+++ Count.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Day_1_opg1
+{
+    public class Count
+    {
+        public int counter;
+
+        public int WordCounter(string maintext, string wordcount)
+        {
+            string[] splittext = maintext.ToLower().Split(' ');
+
+            foreach (string word in splittext)
+            {
+                if (word.Contains(wordcount.ToLower()))
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+    }
+}
